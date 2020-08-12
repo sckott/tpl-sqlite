73a41844b20f215a1bfd4bb8f5253cf6d9117ea0
Scott Chamberlain <myrmecocystus@gmail.com>
2020-08-12 14:04:21 -0700

    update tpl script

--- scripts/theplantlist.R
+++ scripts/theplantlist.R
@@ -1,3 +1,6 @@
+# remove previous version if it exists
+unlink("data/plantlist.sqlite")
+
 library("taxize")
 library("data.table")
 
@@ -9,20 +12,30 @@ df <- rbindlist(lapply(files, fread), fill = TRUE)
 head(df)
 str(df)
 
+# rename columns
+names(df) <- gsub("\\s", "_", tolower(names(df)))
+
+# make full name column
+df$scientificname <- paste(df$genus, df$species)
+
 library("RSQLite")
 library("DBI")
 x <- dbConnect(RSQLite::SQLite(), "data/plantlist.sqlite")
 dbWriteTable(x, "tpl", df)
 dbListTables(x)
 
+# create indices
+RSQLite::dbExecute(x, 'CREATE UNIQUE INDEX id ON tpl (id)')
+RSQLite::dbExecute(x, 'CREATE INDEX sciname ON tpl (scientificname)')
+
 # test it
-dbGetQuery(x, 'SELECT * FROM tpl LIMIT 5')
+# dbGetQuery(x, 'SELECT * FROM tpl LIMIT 5')
 
 # zip the sqlite file
 library("zip")
+unlink("data/plantlist.zip")
 zip::zipr("data/plantlist.zip", "data/plantlist.sqlite",
     include_directories = FALSE)
 
 # cleanup
 dbDisconnect(x)
-unlink("data/plantlist.sqlite")
